b7aad2113b2c2fb7e64b78c9af614c7d09bf4d4e
Raphael Mitsch <laarem33@hotmail.com>
2020-03-13 16:16:29 +0100

    Updated setup.

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,13 +7,6 @@
 # Note: Does this problem still exist with conda base? Adapt setup.sh accordingly.
 #sed -i 's/numba.njit(parallel=True/numba.njit(parallel=False/g' /usr/local/lib/python3.5/site-packages/umap/umap_.py
 
-# Replace sklearn's base.py to avoid error for popping non-existent indices when computing random forests.
-# Fixed by manually applying
-# https://github.com/amueller/scikit-learn/blob/8d640b9a7cab93cde925431948297814d32b31f4/sklearn/base.py.
-# Path in docker container is /usr/local/lib/python3.5/site-packages/sklearn/base.py.
-# Note: Does this problem still exist with conda base? Adapt setup.sh accordingly.
-#cp -f /tmp/base.py /usr/local/lib/python3.5/site-packages/sklearn/base.py
-
 # Download repository and install package for multi-threaded t-SNE.
 pip install --no-cache-dir git+https://github.com/samueljackson92/coranking.git
 
@@ -26,3 +19,4 @@ python -c "import nltk; nltk.download('stopwords'); nltk.download('punkt')"
 git clone https://github.com/facebookresearch/fastText.git
 cd fastText
 pip install .
+cd ..
